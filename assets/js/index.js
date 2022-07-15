205cb18851857e6dff10abf622294979d21e9dcb
NicoRuizDev <lifegamer2050@gmail.com>
2022-07-15 18:13:31 +0530

    added css

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+function linkCopy() {
+    /* Get the text field */
+    let copyText = document.getElementById("fileLink");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+  }
+  
